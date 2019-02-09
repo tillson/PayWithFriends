bc8b5fed15466fee8960d6c22111050d0f267040
Charles Truluck <github@ctruluck.com>
2019-02-08 22:26:06 -0500

    committed for life

---
+++ ios/Pay With Friends/Pay With Friends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pay With Friends
+//
+//  Created by Charles on 2/8/19.
+//  Copyright © 2019 Dangling Pointers LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
