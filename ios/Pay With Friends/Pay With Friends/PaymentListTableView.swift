3876bf8d0a0bea2c7bcaefab80487bb09c88f776
Charles Truluck <github@ctruluck.com>
2019-02-09 11:54:16 -0500

    Updated Layout

---
+++ ios/Pay With Friends/Pay With Friends/PaymentListTableView.swift
@@ -0,0 +1,15 @@
+//
+//  PaymentListTableView.swift
+//  Pay With Friends
+//
+//  Created by Charles on 2/9/19.
+//  Copyright © 2019 Dangling Pointers LLC. All rights reserved.
+//
+
+import UIKit
+
+class PaymentListTableView: UITableViewController {
+    
+    
+    
+}
