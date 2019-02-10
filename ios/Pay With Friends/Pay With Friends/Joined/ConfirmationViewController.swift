07536e5fa650ec0985c65394b18a28f6a28e458d
Charles Truluck <github@ctruluck.com>
2019-02-10 05:04:49 -0500

    confirmation page

--- ios/Pay With Friends/Pay With Friends/Joined/ConfirmationViewController.swift
+++ ios/Pay With Friends/Pay With Friends/Joined/ConfirmationViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ConfirmationViewController: UIViewController {
 
+    @IBOutlet weak var subtotalLabel: UILabel!
+    @IBOutlet weak var taxLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 }
