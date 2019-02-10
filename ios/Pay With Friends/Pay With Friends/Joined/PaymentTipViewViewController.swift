39ccf6580ac7355d4e1dcc901ca492515b635320
Charles Truluck <github@ctruluck.com>
2019-02-10 02:37:58 -0500

    tips

---
+++ ios/Pay With Friends/Pay With Friends/Joined/PaymentTipViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PaymentTipViewViewController.swift
+//  Pay With Friends
+//
+//  Created by Charles on 2/10/19.
+//  Copyright © 2019 Dangling Pointers LLC. All rights reserved.
+//
+
+import UIKit
+
+class PaymentTipViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
